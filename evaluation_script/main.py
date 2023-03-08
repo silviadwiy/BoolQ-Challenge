79f5e9e68e62f7664af4f1b803c07965785d35cf
silviadwiy <117156657+silviadwiy@users.noreply.github.com>
2023-03-08 14:43:55 +0700

    Update main.py

--- evaluation_script/main.py
+++ evaluation_script/main.py
@@ -1,81 +1,57 @@
-import random
-
+import pandas as pd
+# from sklearn.metrics import accuracy_score
 
 def evaluate(test_annotation_file, user_submission_file, phase_codename, **kwargs):
     print("Starting Evaluation.....")
-    """
-    Evaluates the submission for a particular challenge phase and returns score
-    Arguments:
+    print("Submission related metadata:")
 
-        `test_annotations_file`: Path to test_annotation_file on the server
-        `user_submission_file`: Path to file submitted by the user
-        `phase_codename`: Phase to which submission is made
+    # Load the CSV files into pandas dataframes
+    df_true = pd.read_csv(test_annotation_file)
+    df_pred = pd.read_csv(user_submission_file)
 
-        `**kwargs`: keyword arguments that contains additional submission
-        metadata that challenge hosts can use to send slack notification.
-        You can access the submission metadata
-        with kwargs['submission_metadata']
+    # Remove missing rows and duplicates
+    df_true = df_true.dropna().drop_duplicates()
+    df_pred = df_pred.dropna().drop_duplicates()
 
-        Example: A sample submission metadata can be accessed like this:
-        >>> print(kwargs['submission_metadata'])
-        {
-            'status': u'running',
-            'when_made_public': None,
-            'participant_team': 5,
-            'input_file': 'https://abc.xyz/path/to/submission/file.json',
-            'execution_time': u'123',
-            'publication_url': u'ABC',
-            'challenge_phase': 1,
-            'created_by': u'ABC',
-            'stdout_file': 'https://abc.xyz/path/to/stdout/file.json',
-            'method_name': u'Test',
-            'stderr_file': 'https://abc.xyz/path/to/stderr/file.json',
-            'participant_team_name': u'Test Team',
-            'project_url': u'http://foo.bar',
-            'method_description': u'ABC',
-            'is_public': False,
-            'submission_result_file': 'https://abc.xyz/path/result/file.json',
-            'id': 123,
-            'submitted_at': u'2017-03-20T19:22:03.880652Z'
-        }
-    """
+    # Merge the dataframes on the title column
+    df = pd.merge(df_true, df_pred, on='title')
+
+    # Extract the boolean answers from each dataframe
+    correct_answers = df_true["true_answer"].values.tolist()
+    student_answers = df_pred["answer"].values.tolist()
+
+    # Compare student's answers to correct answers and calculate accuracy
+    num_correct = 0
+    for i in range(len(correct_answers)):
+        if student_answers[i] == correct_answers[i]:
+            num_correct += 1
+
+    acc = num_correct / len(correct_answers)
+    
+    print(kwargs["submission_metadata"])
     output = {}
-    if phase_codename == "dev":
-        print("Evaluating for Dev Phase")
+    if phase_codename == "train":
+        print("Evaluating for Train Phase")
         output["result"] = [
             {
                 "train_split": {
-                    "Metric1": random.randint(0, 99),
-                    "Metric2": random.randint(0, 99),
-                    "Metric3": random.randint(0, 99),
-                    "Total": random.randint(0, 99),
+                    "Score": acc,
                 }
             }
         ]
         # To display the results in the result file
         output["submission_result"] = output["result"][0]["train_split"]
-        print("Completed evaluation for Dev Phase")
-    elif phase_codename == "test":
-        print("Evaluating for Test Phase")
+        print("Completed evaluation for Train Phase")
+    elif phase_codename == "dev":
+        print("Evaluating for Dev Phase")
         output["result"] = [
             {
-                "train_split": {
-                    "Metric1": random.randint(0, 99),
-                    "Metric2": random.randint(0, 99),
-                    "Metric3": random.randint(0, 99),
-                    "Total": random.randint(0, 99),
+                "dev_split": {
+                    "Score": acc,
                 }
-            },
-            {
-                "test_split": {
-                    "Metric1": random.randint(0, 99),
-                    "Metric2": random.randint(0, 99),
-                    "Metric3": random.randint(0, 99),
-                    "Total": random.randint(0, 99),
-                }
-            },
+            }
         ]
         # To display the results in the result file
         output["submission_result"] = output["result"][0]
-        print("Completed evaluation for Test Phase")
+        print("Completed evaluation for Dev Phase")
     return output
